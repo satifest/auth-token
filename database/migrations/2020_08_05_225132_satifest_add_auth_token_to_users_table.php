6eca162374954ada42cde7507ff609c4846961bc
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-06 08:09:17 +0800

    Fixes namespace.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2020_08_05_225132_satifest_add_auth_token_to_users_table.php
+++ database/migrations/2020_08_05_225132_satifest_add_auth_token_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Satifest\Foundation\Satifest;
-use function Satifest\table_name;
+use function Satifest\Foundation\table_name;
 
 class SatifestAddAuthTokenToUsersTable extends Migration
 {
