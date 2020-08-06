0b4f54be7090e9d9957dcf1321f0751831cfe631
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-06 08:53:25 +0800

    Add UserObserver.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Observers/UserObserver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Satifest\AuthToken\Observers;
+
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use Illuminate\Support\Str;
+use Satifest\Foundation\Satifest;
+
+class UserObserver
+{
+    /**
+     * Handle the user "creating" event.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function creating(UserContract $user)
+    {
+        $column = Satifest::getAuthTokenName();
+
+        if (\method_exists($user, 'getSatifestAuthToken')) {
+            $user->setAttribute($column, Str::random(60));
+        }
+    }
+
+    /**
+     * Handle the user "updating" event.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function updating(UserContract $user)
+    {
+        $column = Satifest::getAuthTokenName();
+
+        if (\method_exists($user, 'getSatifestAuthToken') && empty($user->getSatifestAuthToken())) {
+            $user->setAttribute($column, Str::random(60));
+        }
+    }
+}
